f27432978acbda3aa73350d605b16b349158c752
Booster39 <abdiame10@yahoo.fr>
2024-10-23 16:03:54 +0200

    feat: password validator improvement

--- front/src/app/features/auth/components/register/register.component.ts
+++ front/src/app/features/auth/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SessionService } from 'src/app/services/session.service';
 import { AuthService } from '../../services/auth.service';
@@ -13,13 +13,30 @@ import { User } from 'src/app/interfaces/user.interface';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-
+  public strongPasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.value;
+      if (!password) {
+        return null;
+      }
+  
+      const hasNumber = /[0-9]/.test(password);
+      const hasLowerCase = /[a-z]/.test(password);
+      const hasUpperCase = /[A-Z]/.test(password);
+      const hasSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+      const isValidLength = password.length >= 8;
+  
+      const isValid = hasNumber && hasLowerCase && hasUpperCase && hasSpecialCharacter && isValidLength;
+      
+      return isValid ? null : { strongPassword: true };
+    };
+  }
   public onError = false;
 
   public form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     name: ['', [Validators.required, Validators.min(3)]],
-    password: ['', [Validators.required, Validators.min(3)]]
+    password: ['', [Validators.required, this.strongPasswordValidator()]]
   });
 
   constructor(private authService: AuthService,
